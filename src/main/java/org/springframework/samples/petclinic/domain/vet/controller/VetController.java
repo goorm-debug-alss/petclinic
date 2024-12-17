f0a26755d6778aced753a67c049cc6963e37c0ff
leedidu <dlwltn510@gmail.com>
2024-12-17 17:32:28 +0900

    feat: 수의사 수정 / 삭제 기능 구현 (#21)

--- src/main/java/org/springframework/samples/petclinic/domain/vet/controller/VetController.java
+++ src/main/java/org/springframework/samples/petclinic/domain/vet/controller/VetController.java
@@ -17,21 +17,38 @@ class VetController {
 
 	private final VetService vetService;
 
+	// 수의사 등록
 	@PostMapping
 	public ResponseEntity<VetResponseDto> create(@RequestBody VetRequestDto vetRequestDto) {
 		var response = vetService.register(vetRequestDto);
 		return ResponseEntity.ok(response);
 	}
 
+	// 전체 수의사 조회
 	@GetMapping
 	public ResponseEntity<List<VetResponseDto>> getAll() {
 		var response = vetService.findAll();
 		return ResponseEntity.ok(response);
 	}
 
+	// 특정 수의사 조회
 	@GetMapping("/{vet-id}")
 	public ResponseEntity<VetResponseDto> getVet(@PathVariable("vet-id") int vetId) {
 		var response = vetService.findById(vetId);
 		return ResponseEntity.ok(response);
+	}
+
+	// 수의사 수정
+	@PutMapping("/{vet-id}")
+	public ResponseEntity<VetResponseDto> update(
+			@PathVariable("vet-id") int vetId, @RequestBody VetRequestDto vetRequestDto){
+		var response = vetService.update(vetId, vetRequestDto);
+		return ResponseEntity.ok(response);
+	}
+
+	// 수의사 삭제
+	@DeleteMapping("/{vet-id}")
+	public void delete(@PathVariable("vet-id") int vetId) {
+		vetService.delete(vetId);
 	}
 }
--- src/main/java/org/springframework/samples/petclinic/domain/vet/service/VetService.java
+++ src/main/java/org/springframework/samples/petclinic/domain/vet/service/VetService.java
@@ -72,4 +72,41 @@ public class VetService {
 			.orElseThrow(() -> new RuntimeException("해당 수의사를 찾을 수 없습니다"));
 		return vetConvert.toResponse(vet);
 	}
+
+	// 수의사 삭제
+	public void delete(int vetId) {
+		vetRepository.deleteById(vetId);
+	}
+
+	// 수의사 수정
+	@Transactional
+	public VetResponseDto update(int id, VetRequestDto vetRequestDto) {
+		Vet vet = vetRepository.findById(id)
+			.orElseThrow(() -> new RuntimeException("해당 수의사를 찾을 수 없습니다"));
+
+		// 이름 수정
+		Optional.ofNullable(vetRequestDto.getName()).ifPresent(vet::setName);
+
+		// 분야 수정
+		Optional.ofNullable(vetRequestDto.getSpecialties()).ifPresent(specialtyDtos -> {
+			vetSpecialityRepository.deleteAllByVetId_Id(vet.getId());
+
+			specialtyDtos.forEach(specialtyDto -> {
+				Specialty specialty = specialityRepository.findByName(specialtyDto.getName())
+					.orElseGet(() -> {
+						Specialty newSpecialty = new Specialty();
+						newSpecialty.setName(specialtyDto.getName());
+						return specialityRepository.save(newSpecialty);
+					});
+
+				// VetSpecialty 생성 / 연결
+				VetSpecialty vetSpecialty = new VetSpecialty();
+				vetSpecialty.setVetId(vet);
+				vetSpecialty.setSpecialtyId(specialty);
+				vetSpecialityRepository.save(vetSpecialty);
+			});
+		});
+		vetRepository.save(vet);
+		return vetConvert.toResponse(vet);
+	}
 }
