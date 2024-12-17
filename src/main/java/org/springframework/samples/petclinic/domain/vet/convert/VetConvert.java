a0fd87c516d0785f9b9bcdb361a21a36f5647715
Jisoo Lee <99534272+leedidu@users.noreply.github.com>
2024-12-17 15:15:31 +0900

    feat: 수의사 등록 기능 추가(#6) (#14)

---
+++ src/main/java/org/springframework/samples/petclinic/domain/vet/convert/VetConvert.java
@@ -0,0 +1,24 @@
+package org.springframework.samples.petclinic.domain.vet.convert;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.domain.vet.controller.dto.VetResponseDto;
+import org.springframework.samples.petclinic.domain.vet.model.Vet;
+import org.springframework.samples.petclinic.speciality.service.SpecialityService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class VetConvert {
+
+	@Autowired
+	private SpecialityService specialityService;
+
+	public VetResponseDto toResponse(Vet vet) {
+		return VetResponseDto.builder()
+			.id(vet.getId())
+			.name(vet.getName())
+			.averageRatings(vet.getAverageRatings())
+			.reviewCount(vet.getReviewCount())
+			.specialties(specialityService.find(vet.getId()))
+			.build();
+	}
+}
--- src/main/java/org/springframework/samples/petclinic/domain/vet/model/Vet.java
+++ src/main/java/org/springframework/samples/petclinic/domain/vet/model/Vet.java
@@ -16,17 +16,18 @@
 package org.springframework.samples.petclinic.domain.vet.model;
 
 import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 import org.springframework.samples.petclinic.model.BaseEntity;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = false)
