90a5e3e2fe644c2d5f864ac23b0a0ca7af12f545
taemin <jungtaemin1214@naver.com>
2024-12-18 15:09:38 +0900

    .

--- src/main/java/org/springframework/samples/petclinic/domain/pet/model/Pet.java
+++ src/main/java/org/springframework/samples/petclinic/domain/pet/model/Pet.java
@@ -1,42 +1,16 @@
-/*
- * Copyright 2012-2019 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.springframework.samples.petclinic.domain.pet.model;
 
-import java.time.LocalDate;
-import java.util.LinkedHashSet;
-import java.util.Set;
-
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.springframework.format.annotation.DateTimeFormat;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
 import org.springframework.samples.petclinic.domain.owner.model.Owner;
-import org.springframework.samples.petclinic.domain.visit.model.Visit;
 import org.springframework.samples.petclinic.model.BaseEntity;
+
+import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
@@ -59,8 +33,4 @@ public class Pet extends BaseEntity {
 	@ManyToOne
 	@JoinColumn(name = "owner_id")
 	private Owner ownerId;
-
-//	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-//	@JoinColumn(name = "pet_id")
-//	private final Set<Visit> visits = new LinkedHashSet<>();
 }
