aec5f21e333032dcbe37890a0595e2b7eaa900d0
HwanSu Kim <68542769+KimHS17@users.noreply.github.com>
2024-12-18 15:58:36 +0900

    feat: 주인 정보 수정 API 추가 (#35)
    * feat: 주인 정보 수정 API 추가
    
    * feat: 비밀번호 변경 API 추가

--- src/main/java/org/springframework/samples/petclinic/domain/owner/controller/OwnerController.java
+++ src/main/java/org/springframework/samples/petclinic/domain/owner/controller/OwnerController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.samples.petclinic.domain.owner.dto.LoginRequestDto;
 import org.springframework.samples.petclinic.domain.owner.dto.RegisterRequestDto;
+import org.springframework.samples.petclinic.domain.owner.dto.UpdatePasswordRequestDto;
+import org.springframework.samples.petclinic.domain.owner.dto.UpdateProfileRequestDto;
 import org.springframework.samples.petclinic.domain.owner.service.OwnerService;
 import org.springframework.samples.petclinic.domain.token.dto.TokenResponseDto;
 import org.springframework.web.bind.annotation.*;
@@ -33,5 +35,19 @@ class OwnerController {
 	public ResponseEntity<TokenResponseDto> tokens() {
 		TokenResponseDto tokenResponseDto = ownerService.tokens();
 		return ResponseEntity.ok(tokenResponseDto);
+	}
+
+	// 정보 수정
+	@PutMapping("/update/{id}")
+	public ResponseEntity updateProfile(@PathVariable Integer id, @RequestBody UpdateProfileRequestDto updateProfileRequestDto) {
+		ownerService.updateProfile(id, updateProfileRequestDto);
+		return ResponseEntity.ok().build();
+	}
+
+	// 비밀번호 변경
+	@PutMapping("/update/password/{id}")
+	public ResponseEntity updatePassword(@PathVariable Integer id, @RequestBody UpdatePasswordRequestDto updatePasswordRequestDto) {
+		ownerService.updatePassword(id, updatePasswordRequestDto);
+		return ResponseEntity.ok().build();
 	}
 }
--- src/main/java/org/springframework/samples/petclinic/domain/owner/service/OwnerService.java
+++ src/main/java/org/springframework/samples/petclinic/domain/owner/service/OwnerService.java
@@ -3,6 +3,8 @@ package org.springframework.samples.petclinic.domain.owner.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.samples.petclinic.domain.owner.dto.LoginRequestDto;
 import org.springframework.samples.petclinic.domain.owner.dto.RegisterRequestDto;
+import org.springframework.samples.petclinic.domain.owner.dto.UpdatePasswordRequestDto;
+import org.springframework.samples.petclinic.domain.owner.dto.UpdateProfileRequestDto;
 import org.springframework.samples.petclinic.domain.owner.model.Owner;
 import org.springframework.samples.petclinic.domain.owner.repository.OwnerRepository;
 import org.springframework.samples.petclinic.domain.token.dto.TokenResponseDto;
@@ -10,6 +12,8 @@ import org.springframework.samples.petclinic.domain.token.service.TokenService;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -48,5 +52,30 @@ public class OwnerService {
 
 		// JWT Token 반환
 		return tokenService.issueToken((Integer) ownerId);
+	}
+
+	public void updateProfile(Integer id, UpdateProfileRequestDto updateProfileRequestDto) {
+		Owner owner = ownerRepository.findById(id)
+			.orElseThrow(() -> new IllegalArgumentException("Owner not found with id: " + id));
+
+		Optional.ofNullable(updateProfileRequestDto.getName()).ifPresent(owner::setName);
+		Optional.ofNullable(updateProfileRequestDto.getAddress()).ifPresent(owner::setAddress);
+		Optional.ofNullable(updateProfileRequestDto.getTelephone()).ifPresent(owner::setTelephone);
+		Optional.ofNullable(updateProfileRequestDto.getCity()).ifPresent(owner::setCity);
+
+		ownerRepository.save(owner);
+	}
+
+	public void updatePassword(Integer id, UpdatePasswordRequestDto updatePasswordRequestDto) {
+		Owner owner = ownerRepository.findById(id)
+			.orElseThrow(() -> new IllegalArgumentException("Owner not found with id: " + id));
+
+		if(!owner.getPassword().equals(updatePasswordRequestDto.getCurrentPassword())) {
+			throw new IllegalArgumentException("Password is not correct");
+		}
+
+		owner.setPassword(updatePasswordRequestDto.getNewPassword());
+
+		ownerRepository.save(owner);
 	}
 }
