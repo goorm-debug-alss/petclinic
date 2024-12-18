1419e01c3ca796da4d21c50c9dcd16cb38c6e17a
KimHS17 <skes01149@naver.com>
2024-12-18 11:52:18 +0900

    feat: 로그인, 회원가입 기능 추가

---
+++ src/main/java/org/springframework/samples/petclinic/domain/owner/repository/OwnerRepository.java
@@ -0,0 +1,12 @@
+package org.springframework.samples.petclinic.domain.owner.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.samples.petclinic.domain.owner.model.Owner;
+
+public interface OwnerRepository extends JpaRepository<Owner, Integer> {
+	Boolean existsByUserId(String userId);
+
+	Optional<Owner> findByUserId(String userId);
+}
---
+++ src/main/java/org/springframework/samples/petclinic/domain/owner/service/OwnerService.java
@@ -0,0 +1,43 @@
+package org.springframework.samples.petclinic.domain.owner.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.samples.petclinic.domain.owner.dto.LoginRequestDto;
+import org.springframework.samples.petclinic.domain.owner.dto.RegisterRequestDto;
+import org.springframework.samples.petclinic.domain.owner.model.Owner;
+import org.springframework.samples.petclinic.domain.owner.repository.OwnerRepository;
+import org.springframework.samples.petclinic.domain.token.dto.TokenResponseDto;
+import org.springframework.samples.petclinic.domain.token.service.TokenService;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+
+@Service
+@RequiredArgsConstructor
+public class OwnerService {
+	private final OwnerRepository ownerRepository;
+	private final TokenService tokenService;
+
+	public void register(RegisterRequestDto registerRequestDto) {
+		boolean isExist = ownerRepository.existsByUserId(registerRequestDto.getUserId());
+
+		if(isExist) {
+			throw new IllegalArgumentException("Owner already exists with userId: " + registerRequestDto.getUserId());
+		}
+
+		Owner owner = registerRequestDto.toEntity();
+		ownerRepository.save(owner);
+	}
+
+	public TokenResponseDto login(LoginRequestDto loginRequestDto) {
+		Owner owner = ownerRepository.findByUserId(loginRequestDto.getUserId())
+			.orElseThrow(() -> new IllegalArgumentException("Owner not found with userId: " + loginRequestDto.getUserId()));
+
+		// 비밀번호 검증
+		if(!owner.getPassword().equals(loginRequestDto.getPassword())) {
+			throw new IllegalArgumentException("Password is not correct");
+		}
+
+		// JWT Token 반환
+		return tokenService.issueToken(owner.getId());
+	}
+}
