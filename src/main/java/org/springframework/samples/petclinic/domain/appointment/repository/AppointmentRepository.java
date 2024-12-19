bf86da019ac367ffee3052ce9e3f7ccd1fab3aff
gurunee <119057831+popcifox@users.noreply.github.com>
2024-12-19 09:44:47 +0900

    Delete src/main/java/org/springframework/samples/petclinic/domain/appointment directory

--- src/main/java/org/springframework/samples/petclinic/domain/appointment/repository/AppointmentRepository.java
+++
@@ -1,7 +0,0 @@
-package org.springframework.samples.petclinic.domain.appointment.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.samples.petclinic.domain.appointment.model.Appointment;
-
-public interface AppointmentRepository extends JpaRepository<Appointment, Integer> {
-}
