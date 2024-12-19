62f97b70dfa90b705d67119993b78975349b6659
Jisoo Lee <99534272+leedidu@users.noreply.github.com>
2024-12-19 17:05:56 +0900

    feat: 수의사 관련 테스트 코드 추가 (#34) (#55)

--- src/main/java/org/springframework/samples/petclinic/domain/vet/controller/VetController.java
+++ src/main/java/org/springframework/samples/petclinic/domain/vet/controller/VetController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/vets")
-class VetController {
+public class VetController {
 
 	private final VetService vetService;
 
