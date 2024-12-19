07424d3db196a5b680bb462df24a47d65e5b281e
Do-oya <qkrwjdgus1441@naver.com>
2024-12-19 13:37:49 +0900

    refactor: 테스트용 Repository 삭제후 사용코드 리팩토링
    - GarbagePetRepository -> PetRepository
    - GarbageVetRepository -> VetRepository

--- src/main/java/org/springframework/samples/petclinic/domain/appointment/garbage/GarbagePetRepository.java
+++
@@ -1,7 +0,0 @@
-package org.springframework.samples.petclinic.domain.appointment.garbage;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.samples.petclinic.domain.pet.model.Pet;
-// 테스트용 입니다. PetRepository 구현되면 바로 삭제하겠습니다.
-public interface GarbagePetRepository extends JpaRepository<Pet, Integer> {
-}
