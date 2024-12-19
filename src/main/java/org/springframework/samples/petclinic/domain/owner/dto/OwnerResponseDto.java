84670876f6a34e3f42be2a1c5f1fcde6dddb99ef
HwanSu Kim <68542769+KimHS17@users.noreply.github.com>
2024-12-19 11:16:38 +0900

    feat: 주인 조회 관련 API 추가 (#45)
    * feat: 주인 목록 조회 API 추가
    
    * feat: 특정 주인 조회 API 추가

---
+++ src/main/java/org/springframework/samples/petclinic/domain/owner/dto/OwnerResponseDto.java
@@ -0,0 +1,17 @@
+package org.springframework.samples.petclinic.domain.owner.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OwnerResponseDto {
+	private String name;
+	private String address;
+	private String city;
+	private String telephone;
+}
