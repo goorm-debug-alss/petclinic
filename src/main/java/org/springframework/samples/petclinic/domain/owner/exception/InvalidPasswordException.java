74ba45929a756498218ec85b1f0fd939363a45a3
Do-oya <qkrwjdgus1441@naver.com>
2024-12-23 15:50:20 +0900

    feat: 커스텀 Exception 구현

---
+++ src/main/java/org/springframework/samples/petclinic/domain/owner/exception/InvalidPasswordException.java
@@ -0,0 +1,7 @@
+package org.springframework.samples.petclinic.domain.owner.exception;
+
+public class InvalidPasswordException extends RuntimeException {
+	public InvalidPasswordException(String message) {
+		super(message);
+	}
+}
