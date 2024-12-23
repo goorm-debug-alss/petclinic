2cd0d33e9e08439129de6d90895e480f260269c3
Junhwan Song <140828021+junhwan98@users.noreply.github.com>
2024-12-23 15:45:50 +0900

    feat: 방문 생성,조회 API 테스트코드 작성(#68) (#69)
    * test: VisitService 테스트코드 작성
    
    * test: VisitController 테스트코드 작성
    
    * docs: 테스트코드 주석 수정

--- src/main/java/org/springframework/samples/petclinic/domain/visit/dto/VisitRequestDto.java
+++ src/main/java/org/springframework/samples/petclinic/domain/visit/dto/VisitRequestDto.java
@@ -1,11 +1,15 @@
 package org.springframework.samples.petclinic.domain.visit.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class VisitRequestDto {
 
 	private int petId;
