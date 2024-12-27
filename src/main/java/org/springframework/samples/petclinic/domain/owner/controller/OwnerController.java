aaeaed4ec3f374001aa729c364519a378a5d1885
Park JeongHyun <qkrwjdgus1441@naver.com>
2024-12-27 18:19:19 +0900

    refactor: 주석 제거(#93) (#96)
    refactor: 주석 제거

--- src/main/java/org/springframework/samples/petclinic/domain/owner/controller/OwnerController.java
+++ src/main/java/org/springframework/samples/petclinic/domain/owner/controller/OwnerController.java
@@ -14,10 +14,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-/**
- * 주인 관련 요청을 처리하는 컨트롤러
- * - 이 클래스는 회원가입 및 로그인, 토큰 발급, 조회 및 수정 요청을 처리하며, 서비스 계층과 연결되어 비즈니스 로직을 수행
- */
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/owner")
@@ -29,36 +25,21 @@ class OwnerController {
 	private final TokenService tokenService;
 	private final BlacklistService blacklistService;
 
-	/**
-	 * 회원가입 API
-	 *
-	 * @param registerRequestDto 클라이언트로부터 전달받은 회원가입 요청 데이터
-	 * @return 성공 시 상태 코드 200 OK 반환
-	 */
+	// 회원가입
 	@PostMapping("/register")
 	public ResponseEntity register(@RequestBody RegisterRequestDto registerRequestDto) {
 		ownerAuthService.register(registerRequestDto);
 		return ResponseEntity.ok().build();
 	}
 
-	/**
-	 * 로그인 API
-	 *
-	 * @param loginRequestDto 클라이언트로부터 전달받은 로그인 요청 데이터
-	 * @return 인증 토큰을 포함하는 응답 DTO 반환
-	 */
+	// 로그인
 	@PostMapping("/login")
 	public ResponseEntity<TokenResponseDto> login(@RequestBody LoginRequestDto loginRequestDto) {
 		TokenResponseDto tokenResponseDto = ownerAuthService.login(loginRequestDto);
 		return ResponseEntity.ok(tokenResponseDto);
 	}
 
-	/**
-	 * 로그아웃 API
-	 *
-	 * @param request Http 요청 객체
-	 * @return 로그아웃 처리 결과
-	 */
+	// 로그아웃
 	@PostMapping("/logout")
 	public ResponseEntity<?> logout(HttpServletRequest request) {
 		String token = tokenService.extractToken(request);
@@ -70,59 +51,34 @@ class OwnerController {
 		return ResponseEntity.ok("Logged out successfully");
 	}
 
-	/**
-	 * 토큰 재발급 API
-	 *
-	 * @return 재발급된 인증 토큰을 포함하는 응답 DTO 반환
-	 */
+	// 토큰 재발급
 	@GetMapping("/tokens")
 	public ResponseEntity<TokenResponseDto> tokens() {
 		TokenResponseDto tokenResponseDto = ownerAuthService.tokens();
 		return ResponseEntity.ok(tokenResponseDto);
 	}
 
-	/**
-	 * 회원 정보 수정 API
-	 *
-	 * @param id 수정할 회원의 고유 ID
-	 * @param updateProfileRequestDto 클라리언트로부터 전달받은 회원 정보 수정 요청 데이터
-	 * @return 성공 시 상태 코드 200 OK 반환
-	 */
+	// 회원정보 수정
 	@PutMapping("/update/{ownerId}")
 	public ResponseEntity updateProfile(@PathVariable("ownerId") Integer id, @RequestBody UpdateProfileRequestDto updateProfileRequestDto) {
 		ownerProfileService.updateProfile(id, updateProfileRequestDto);
 		return ResponseEntity.ok().build();
 	}
 
-	/**
-	 * 비밀번호 변경 API
-	 *
-	 * @param id 변경할 회원의 고유 ID
-	 * @param updatePasswordRequestDto 클라이언트로부터 전달받은 비밀번호 변경 요청 데이터
-	 * @return 성공 시 상태 코드 200 OK 반환
-	 */
+	// 비밀번호 변경
 	@PutMapping("/update/password/{ownerId}")
 	public ResponseEntity updatePassword(@PathVariable("ownerId") Integer id, @RequestBody UpdatePasswordRequestDto updatePasswordRequestDto) {
 		ownerProfileService.updatePassword(id, updatePasswordRequestDto);
 		return ResponseEntity.ok().build();
 	}
 
-	/**
-	 * 회원 목록 조회 API
-	 *
-	 * @return 모든 회원 정보를 포함하는 응답 DTO 목록 반환
-	 */
+	// 회원 목록 조회
 	@GetMapping
 	public ResponseEntity<List<OwnerResponseDto>> findAll() {
 		return ResponseEntity.ok(ownerReadService.findAll());
 	}
 
-	/**
-	 * 특정 회원 조회 API
-	 *
-	 * @param id 조회할 회원의 고유 ID
-	 * @return 해당 회원의 상세 정보를 포함하는 응답 DTO 반환
-	 */
+	// 특정 회원 조회
 	@GetMapping("/{ownerId}")
 	public ResponseEntity<OwnerResponseDto> findById(@PathVariable("ownerId") Integer id) {
 		return ResponseEntity.ok(ownerReadService.findById(id));
--- src/main/java/org/springframework/samples/petclinic/domain/owner/service/OwnerReadService.java
+++ src/main/java/org/springframework/samples/petclinic/domain/owner/service/OwnerReadService.java
@@ -10,31 +10,18 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 
-/**
- * 주인 조회 서비스
- * - 주인 정보를 조회하고 응답 객체를 반환
- */
 @Service
 @RequiredArgsConstructor
 public class OwnerReadService {
 
 	private final OwnerRepository ownerRepository;
 
-	/**
-	 * 모든 회원 데이터를 조회
-	 *
-	 * @return OwnerResponseDto 각 회원의 이름, 주소, 전화번호, 도시 정보를 포함
-	 */
+	// 모든 회원 데이터를 조회
 	public List<OwnerResponseDto> findAll() {
 		return findAllOwners();
 	}
 
-	/**
-	 * ID로 특정 회원 데이터 조회
-	 *
-	 * @param id 조회할 회원의 고유 ID
-	 * @return OwnerResponseDto 해당 회원의 이름, 주소, 전화번호, 도시 정보를 포함
-	 */
+	// 특정 회원 조회
 	public OwnerResponseDto findById(Integer id) {
 		Owner owner = findOwnerByIdOrThrow(id);
 
