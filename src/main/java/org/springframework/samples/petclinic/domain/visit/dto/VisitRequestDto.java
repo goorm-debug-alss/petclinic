e0d65355d274a2aeb7691e80d2ad9a42350b9d2b
junhwan98 <gensh666@naver.com>
2025-01-07 01:49:31 +0900

    feat: 방문 요청데이터 검증 예외처리 및 불필요한 코드 삭제

--- src/main/java/org/springframework/samples/petclinic/domain/visit/dto/VisitRequestDto.java
+++ src/main/java/org/springframework/samples/petclinic/domain/visit/dto/VisitRequestDto.java
@@ -1,5 +1,9 @@
 package org.springframework.samples.petclinic.domain.visit.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,7 +16,14 @@ import java.time.LocalDateTime;
 @Builder
 public class VisitRequestDto {
 
+	@NotNull(message = "Pet ID는 필수입니다.")
 	private int petId;
+
+	@NotNull(message = "방문 날짜는 필수입니다.")
+	@PastOrPresent(message = "방문 시간은 현재시간보다 미래일 수 없습니다.")
 	private LocalDateTime visitDate;
+
+	@NotBlank(message = "설명은 필수입니다.")
+	@Size(max = 255, message = "설명은 255byte를 초과할 수 없습니다.")
 	private String description;
 }
