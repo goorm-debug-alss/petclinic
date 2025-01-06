00e7d0bae1504cce4d48ebfb1984ea411e96b58c
junhwan98 <gensh666@naver.com>
2025-01-07 02:00:48 +0900

    feat: 진료내역 요청데이터 검증 구현

--- src/main/java/org/springframework/samples/petclinic/domain/visit/dto/VisitRequestDto.java
+++ src/main/java/org/springframework/samples/petclinic/domain/visit/dto/VisitRequestDto.java
@@ -16,14 +16,14 @@ import java.time.LocalDateTime;
 @Builder
 public class VisitRequestDto {
 
-	@NotNull(message = "Pet ID는 필수입니다.")
+	@NotNull(message = "PetID는 필수입니다.")
 	private int petId;
 
-	@NotNull(message = "방문 날짜는 필수입니다.")
-	@PastOrPresent(message = "방문 시간은 현재시간보다 미래일 수 없습니다.")
+	@NotNull(message = "방문일시는 필수입니다.")
+	@PastOrPresent(message = "방문일시는 현재보다 미래일 수 없습니다.")
 	private LocalDateTime visitDate;
 
-	@NotBlank(message = "설명은 필수입니다.")
-	@Size(max = 255, message = "설명은 255byte를 초과할 수 없습니다.")
+	@NotBlank(message = "내용은 필수입니다.")
+	@Size(max = 255, message = "내용은 255byte를 초과할 수 없습니다.")
 	private String description;
 }
