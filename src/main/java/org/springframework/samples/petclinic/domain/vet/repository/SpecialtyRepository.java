a740bd61a2d6d48e1529bd3c832d56e2d19a8776
leedidu <dlwltn510@gmail.com>
2025-01-09 17:14:58 +0900

    refactor: 불필요한 import 삭제 (#133)

--- src/main/java/org/springframework/samples/petclinic/domain/vet/repository/SpecialtyRepository.java
+++ src/main/java/org/springframework/samples/petclinic/domain/vet/repository/SpecialtyRepository.java
@@ -3,7 +3,5 @@ package org.springframework.samples.petclinic.domain.vet.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.samples.petclinic.domain.vet.model.Specialty;
 
-import java.util.Optional;
-
 public interface SpecialtyRepository extends JpaRepository<Specialty, Integer> {
 }
